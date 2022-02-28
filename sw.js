2b0649353088594d6ed6865f78c824bbdad546b3
tomrr6 <tomrr6@hotmail.com>
2022-02-28 16:52:30 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+var PRECACHE_URLS = [
+  'index.html',
+  './', //alias for index.html
+  '/index.html',
+  'css/style.css',
+  'js/main.js',
+  'images/hello-icon-128.png',
+  'images/hello-icon-144.png',
+  'images/hello-icon-152.png',
+  'images/hello-icon-192.png',
+  'images/hello-icon-196maskable.png',
+  'images/hello-icon-256.png',
+  'images/hello-icon-512.png',
+  'favicon.ico',
+  'sw.js'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName.then(funtion(cache) {
+                               return cache.addAll(filesToCache);
+                })
+  );
+});
+
+// serve cached content when offline
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(funtion(response){
+                                 return response || fetch(e.request);
+    })
+);
+});

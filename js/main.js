386e92648c2a00889b9eb46027b6ffb1517859a9
tomrr6 <tomrr6@hotmail.com>
2022-02-28 17:13:44 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 // window.onLoad = () => {
 //   'use strict';
-
+console.log('1');
 function regSW(){
+  console.log('2');
   if('serviceWorker' in navigator) {
+    console.log('3');
     navigator.serviceWorker.register('sw.js');
   }
 }
